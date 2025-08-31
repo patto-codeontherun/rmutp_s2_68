ebb91262df19a9a9aa8a478f950ea47f4ac376d1
patto-codeontherun <pushto168@gmail.com>
2025-08-31 15:47:12 +0700

    Stop tracking dist folder

--- dist/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const hono_1 = require("hono");
-const app = new hono_1.Hono();
-app.get("/", (c) => c.text("Hello World"));
-exports.default = app;
